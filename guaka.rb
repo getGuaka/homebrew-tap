7ccc582b659e38017ae2438a5a8cd6f3902d7528
Thi <t@thi.im>
2018-08-18 10:06:04 +0900

    Initial commit

---
+++ guaka.rb
@@ -0,0 +1,9 @@
+class Guaka < Formula
+  url "https://github.com/getGuaka/Guaka-Generator/releases/download/0.1.1/guaka-generator-0.1.1-darwin-X64.tar.bz2"
+  version "0.1.1"
+  sha256 "e7e610855cf287e01e231182af0192c0d74560913851e344d627db92ab452036"
+
+  def install
+    bin.install "guaka"
+  end
+end
